b45329cae2c01b96cc8c69b440072a07466fd79d
Leesw_0811 <s2018s15@e-mirim.hs.kr>
2020-04-03 23:58:20 +0900

    main 수정 🔨

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css" href="./CSS/index.css?ver=1.2">
+    <link rel="stylesheet" type="text/css" href="./CSS/index.css?ver=1.3">
+    <link rel="stylesheet" type="text/css" href="./CSS/index-ani.css?ver=1.1">
     <link rel="shortcut icon" href="IMG/Icon/favicon___.png">
     <link rel="icon" href="IMG/Icon/favicon___.png">
 
@@ -57,19 +58,30 @@
                 <p><span id="chat">챗봇</span>과 대화하며</p>
                 <p>미림에 대해 더 알아보세요!</p>
             </div>
-            <hr style="width: 550px; margin-top: -20px;">
+            <!-- <hr style="width: 550px; margin-top: -20px;"> -->
             <div class="Cont-cont2">
                 <p>미림을 다녀본 선배가 미림을 다니며 필요한 것을 여기에 담았다!</p>
                 <p>함께 미림의 모든 것을 알아보자!</p>
             </div>
-        
+            <img id="seeMore" src="img/Main/SeeMore.png">
         </div>
 
-        <img id="IMG1" class="tilt-in-top-1" src="IMG/Main/content_01.png">
-        <img id="IMG2" class="tilt-in-top-2" src="IMG/Main/content_02.png">
+
+        <div class="right-content">
+            <img id="IMG2" class="tilt-in-top-2" src="IMG/Main/RP/people.png">
+            <img class="title-in-top-2-1 slide-top" src="IMG/Main/RP/bubble1.png">
+            <img class="title-in-top-2-2 slide-top" src="IMG/Main/RP/bubble2.png">
+            <img class="title-in-top-2-3 slide-tr" src="IMG/Main/RP/air.png">
+        </div>
+
+        <div class="left-content">              
+            <img id="IMG1" class="tilt-in-top-1" src="IMG/Main/LP/people.png">
+            <img class="title-in-top-1 slide-top" src="IMG/Main/LP/bubble.png">  
+            <img class="title-in-top-1-2" src="IMG/Main/LP/deco.png">
+        </div>
 
         <a href="index.php"><img id="Logo" src="IMG/Main/logo.png"></a>
-        <img id="BG" src="IMG/Main/BG_01.png">
+        <img id="BG" src="IMG/Main/BG.png">
     </div>
 </body>
 </html>
