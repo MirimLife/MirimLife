3ee1de54886f2db0278a156bede79165caf482c2
Leesw_0811 <s2018s15@e-mirim.hs.kr>
2020-04-12 02:16:03 +0900

    index 정말 마지막 이기를..

--- index.php
+++ index.php
@@ -21,9 +21,10 @@
     <title>MirimLife</title>
 </head>
 <body style = "position: fixed;" oncontextmenu='return false' onselectstart='return false' ondragstart='return false'>
-    <!-- <div id="BG">
+   
+    <div id="BG">
         <img src="IMG/Main/BG.png" id="BG-img">
-    </div> -->
+    </div>
 
     <div id="wrapper">
         <!-- 메뉴 바 -->
@@ -82,7 +83,7 @@
                     <p>미림을 다녀본 선배가 미림을 다니며 필요한 것을 여기에 담았다!</p>
                     <p>함께 미림의 모든 것을 알아보자!</p>
                 </div>
-                <img id="seeMore" src="IMG/Main/SeeMore.png">
+                <img id="seeMore" src="img/Main/SeeMore.png">
             </div>
 
             <div class="content-Right">
